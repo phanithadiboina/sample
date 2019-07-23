805c6c5802abbce7510f75041a2091875bcde6da
siva <phanithadiboina245@gmail.com>
2019-07-22 22:39:22 -0400

    committing command script

---
+++ Command.bat
@@ -0,0 +1,2 @@
+pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin"
+MSBuild.exe F:\Builds\Code\SampleApplication\SampleApplication.sln /t:ReBuild /p:Configuration=Debug
